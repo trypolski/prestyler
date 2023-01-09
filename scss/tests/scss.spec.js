650ead88a0a55f3839b2ca9eec15408cb301f3b1
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2023-01-08 23:27:44 -0600

    Improved generate-classes utility mixin, added jest tests for mixins and functions

---
+++ scss/tests/scss.spec.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const sassTrue = require('sass-true');
+const glob = require('glob');
+
+const TEST_PATH = `**/*.test.scss`;
+
+describe('Sass', () => {
+  glob
+    .sync(TEST_PATH, { cwd: __dirname, absolute: true })
+    .forEach((file) => sassTrue.runSass({ file }, { describe, it } ));
+});
