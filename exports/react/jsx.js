886a36e887f816271308bb7ee21a3fa3933972f3
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2025-06-12 22:41:30 -0500

    TASK-273 Added ButtonBaseComponent, PrimaryButton components. Implemented jest test.

--- exports/react/jsx.js
+++ exports/react/jsx.js
@@ -1,1 +1,1 @@
-export { default as PrimaryButton } from "../src/react-jsx/components/buttons/PrimaryButton";
+export { default as PrimaryButton } from "../../src/react-jsx/components/buttons/PrimaryButton/PrimaryButton";
