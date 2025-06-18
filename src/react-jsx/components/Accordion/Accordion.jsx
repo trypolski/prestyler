facca743613b2c73592c2afb2fcd302a94c7ff50
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2025-06-17 23:50:33 -0500

    TASK-275 Fixed accordion item CSS transitions. Added tests for Accordion and AccordionItem components. Added more stories for Accordion.

--- src/react-jsx/components/Accordion/Accordion.jsx
+++ src/react-jsx/components/Accordion/Accordion.jsx
@@ -3,20 +3,22 @@ import { usePrestylerPrefix } from "../../hooks/usePrestylerPrefix";
 
 const AccordionContext = createContext();
 
-export default function({ 
+export default function({
   children,
   allowMultiple = false,
   className = "",
   useBsClasses = true,
+  defaultOpenItems = [],
+  ...restProps
 }) {
   const prefix = usePrestylerPrefix();
-  const [openItems, setOpenItems] = useState([]);
+  const [openItems, setOpenItems] = useState(defaultOpenItems);
 
   const toggleItem = (id) => {
     setOpenItems((prevOpenItems) => {
       if (allowMultiple) {
-        return prevOpenItems.includes(id) 
-          ? prevOpenItems.filter((i) => i !== id) 
+        return prevOpenItems.includes(id)
+          ? prevOpenItems.filter((i) => i !== id)
           : [...prevOpenItems, id];
       } else {
         return prevOpenItems.includes(id) ? [] : [id];
@@ -24,11 +26,14 @@ export default function({
     });
   };
 
+  const isItemOpen = (id) => openItems.includes(id);
+
   return (
-    <AccordionContext.Provider value={{ openItems, toggleItem }}>
-      <div 
-        className={`${useBsClasses ? `${prefix}accordion` : ''} ${className}`.trim()} 
+    <AccordionContext.Provider value={{ openItems, toggleItem, isItemOpen }}>
+      <div
         role="presentation"
+        {...restProps}
+        className={`${useBsClasses ? `${prefix}accordion` : ''} ${className}`.trim()}
       >
         {children}
       </div>
--- src/react-jsx/components/Accordion/AccordionItem.jsx
+++ src/react-jsx/components/Accordion/AccordionItem.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import { useAccordion } from './Accordion';
 import { usePrestylerPrefix } from "../../hooks/usePrestylerPrefix";
 
-export default function({ 
+export default function({
   id,
   title,
   children,
@@ -11,57 +11,107 @@ export default function({
   buttonClassName = '',
   collapseClassName = '',
   bodyClassName = '',
+  itemTestId = `accordion-item-${id}`,
+  titleTestId = `accordion-item-title-${id}`,
+  buttonTestId = `accordion-item-button-${id}`,
+  collapseTestId = `accordion-item-collapse-${id}`,
+  bodyTestId = `accordion-item-body-${id}`,
   useBsClasses = true,
+  onClick = () => {},
 }) {
   const prefix = usePrestylerPrefix();
-  const { openItems, toggleItem } = useAccordion();
-  const isOpen = openItems.includes(id);
+  const { toggleItem, isItemOpen } = useAccordion();
+  const collapseRef = useRef(null);
+  const [init, setInit] = useState(true);
+  const isOpen = isItemOpen(id);
 
-  const classesByAccordionElement = {
-    item: `${prefix}accordion-item`,
-    title: `${prefix}accordion-header`,
-    button: `${prefix}accordion-button`,
-    collapse: `${prefix}accordion-collapse`,
-    body: `${prefix}accordion-body`,
+  useEffect(() => {
+    const collapseEl = collapseRef.current;
+    if (!collapseEl) return;
+    if (init && !isOpen) {
+      setInit(false);
+      return;
+    }
+    function transitionEndListener() {
+      if (isOpen) {
+        collapseEl.classList.remove(`${prefix}collapsing`);
+        collapseEl.classList.add(`${prefix}collapse`, `${prefix}show`);
+        collapseEl.style.height = '';
+      } else {
+        collapseEl.classList.remove(`${prefix}collapsing`);
+        collapseEl.classList.add(`${prefix}collapse`);
+        collapseEl.style.height = '';
+      }
+      collapseEl.removeEventListener('transitionend', transitionEndListener);
+    }
+    collapseEl.addEventListener('transitionend', transitionEndListener);
+
+    if (isOpen) {
+      collapseEl.classList.remove(`${prefix}collapse`);
+      collapseEl.classList.add(`${prefix}collapsing`);
+      collapseEl.style.height = '0px';
+      collapseEl.offsetHeight;
+      collapseEl.style.height = collapseEl.scrollHeight + 'px';
+    } else {
+      collapseEl.style.height = collapseEl.scrollHeight + 'px';
+      collapseEl.offsetHeight;
+      collapseEl.classList.remove(`${prefix}collapse`, `${prefix}show`);
+      collapseEl.classList.add(`${prefix}collapsing`);
+      collapseEl.style.height = '0px';
+    }
+  }, [isOpen]);
+
+  function handleItemClick(e) {
+    toggleItem(id);
+    onClick(e, id);
   }
 
-  const customClassesOfAccordionElement = {
-    item: itemClassName,
-    title: titleClassName,
-    button: buttonClassName,
-    collapse: collapseClassName,
-    body: bodyClassName,
+  let fullItemClasses = itemClassName;
+  let fullTitleClasses = titleClassName;
+  let fullButtonClasses = buttonClassName;
+  let fullCollapseClasses = collapseClassName;
+  let fullBodyClasses = bodyClassName;
+  if (useBsClasses) {
+    fullItemClasses = `${prefix}accordion-item ${itemClassName}`.trim();
+    fullTitleClasses = `${prefix}accordion-header ${titleClassName}`.trim();
+    fullButtonClasses = `${prefix}accordion-button ${buttonClassName}`.trim();
+    fullCollapseClasses = `${prefix}accordion-collapse ${prefix}collapse ${collapseClassName}`.trim();
+    fullBodyClasses = `${prefix}accordion-body ${bodyClassName}`.trim();
   }
-
-  const accordionElements = Object.keys(classesByAccordionElement);
-  const fullClassesByElement = accordionElements.reduce((fullClassesObj, elementName) => {
-    const customClassesOfElement = customClassesOfAccordionElement[elementName];
-    const fullClassName = useBsClasses
-      ? `${classesByAccordionElement[elementName]} ${customClassesOfElement}`
-      : customClassesOfElement;
-    fullClassesObj[elementName] = fullClassName.trim();
-    return fullClassesObj;
-  }, {});
 
   return (
-    <div className={fullClassesByElement.item}>
-      <h2 className={fullClassesByElement.title}>
+    <div 
+      className={fullItemClasses}
+      data-testid={itemTestId}
+    >
+      <h2 
+        className={fullTitleClasses}
+        data-testid={titleTestId}
+      >
         <button
-          className={`${fullClassesByElement.button} ${isOpen ? '' : `${prefix}collapsed`}`}
+          className={`${fullButtonClasses} ${isOpen ? '' : `${prefix}collapsed`}`}
           type="button"
-          onClick={() => toggleItem(id)}
+          onClick={handleItemClick}
           aria-expanded={isOpen}
           aria-controls={`panel-${id}`}
+          data-testid={buttonTestId}
         >
           {title}
         </button>
       </h2>
       <div
         id={`panel-${id}`}
-        className={`${fullClassesByElement.collapse} ${prefix}collapse ${isOpen ? `${prefix}show` : ''}`}
+        ref={collapseRef}
+        className={fullCollapseClasses}
         aria-labelledby={`heading-${id}`}
+        data-testid={collapseTestId}
       >
-        <div className={fullClassesByElement.body}>{children}</div>
+        <div
+          className={fullBodyClasses}
+          data-testid={bodyTestId}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
