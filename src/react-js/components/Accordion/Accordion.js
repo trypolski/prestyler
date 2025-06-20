7646529bd70a83954d9a73cb7809de5d1058edef
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2025-06-20 12:49:17 -0500

    TASK-304 Added test cases for toggle buttons, created JS component for toggle button, fixed Accordtion React import error.

--- src/react-js/components/Accordion/Accordion.js
+++ src/react-js/components/Accordion/Accordion.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useMemo } from 'react';
+import React, { createContext, useState, useContext, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { usePrestylerPrefix } from '../../hooks/usePrestylerPrefix';
 
--- src/react-js/components/buttons/Button/Button.js
+++ src/react-js/components/buttons/Button/Button.js
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 import ButtonBaseComponent from '../ButtonBaseComponent/ButtonBaseComponent';
 import { BUTTON_CLASSES, BUTTON_SIZES } from '../constants';
 
-export default function Button({ variant = 'default', isLarge, isSmall, isOutlined, ...props }) {
+export default function Button({
+  variant = 'default',
+  isLarge,
+  isSmall,
+  isOutlined,
+  isToggleable,
+  isActive,
+  ...props
+}) {
   let bsClasses = ['btn'];
   if (variant !== 'default') {
     bsClasses.push(BUTTON_CLASSES[variant]);
@@ -11,8 +19,15 @@ export default function Button({ variant = 'default', isLarge, isSmall, isOutlin
   if (isOutlined) bsClasses = bsClasses.map((cls) => cls.replace('btn-', 'btn-outline-'));
   if (isLarge) bsClasses.push(BUTTON_SIZES.large);
   if (isSmall) bsClasses.push(BUTTON_SIZES.small);
+  if (isToggleable && isActive) bsClasses.push('active');
 
-  return <ButtonBaseComponent {...props} bsClasses={bsClasses.join(' ')} />;
+  return (
+    <ButtonBaseComponent
+      {...props}
+      {...(isToggleable ? { 'data-bs-toggle': 'button', 'aria-pressed': isActive } : {})}
+      bsClasses={bsClasses.join(' ')}
+    />
+  );
 }
 
 Button.propTypes = {
@@ -20,4 +35,6 @@ Button.propTypes = {
   isLarge: PropTypes.bool,
   isSmall: PropTypes.bool,
   isOutlined: PropTypes.bool,
+  isToggleable: PropTypes.bool,
+  isActive: PropTypes.bool,
 };
