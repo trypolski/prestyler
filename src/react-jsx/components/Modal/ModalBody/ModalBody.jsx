4674aa4d226789d4e6a4cd14096c580c8c076687
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2025-07-22 22:03:46 -0500

    TASK-286 Added ModalBody, ModalFooter, ModalHeader, and ModalTitle components. Added new properties to Modal dialog component

---
+++ src/react-jsx/components/Modal/ModalBody/ModalBody.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Wrapper from '../../common/Wrapper/Wrapper';
+
+export default function ModalBody(props) {
+  return <Wrapper {...props} wrapperClass="modal-body" />;
+}
---
+++ src/react-jsx/components/Modal/ModalFooter/ModalFooter.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Wrapper from '../../common/Wrapper/Wrapper';
+
+export default function ModalFooter(props) {
+  return <Wrapper {...props} wrapperClass="modal-footer" />;
+}
