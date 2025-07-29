5be545aabfc3f8bccb73d78ef76370d85abed5f1
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2025-07-28 23:52:22 -0500

    TASK-287 Added li, section, article to Wrapper common component. Added navigation collapse prop to reuse AccordtionItem component as navigation collapse. Added Navbar component. Added NavbarBrand component. Added NavbarCollapse component. Added NavbarList component. Added NavbarListItem component. Added NavbarToggle component.

---
+++ src/react-jsx/components/Navbar/NavbarList/NavbarList.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Wrapper from '../../common/Wrapper/Wrapper';
+
+export default function NavbarList(props) {
+  return <Wrapper {...props} tag="ul" wrapperClass="navbar-nav" />;
+}
